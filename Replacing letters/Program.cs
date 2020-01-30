61f9208cab5e46db5b14414f25b1ab3a530aacbd
Kokojam7a <Kokojam7a@DESKTOP-2JBLTON>
2020-01-30 18:29:40 +0200

    Добавьте файлы проекта.

---
+++ Replacing letters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replacing_letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] buff = (Console.ReadLine() +" ").ToCharArray();
+            int start = 0;
+            for (int i = 0; i < buff.Length; i++)
+            if (!char.IsLetterOrDigit(buff[i])) { 
+            Array.Reverse(buff, start, i - start);
+            start = i + 1; }
+            Console.WriteLine(new String(buff, 0, buff.Length - 1));
+            Console.ReadLine();
+
+
+        }
+    }
+}
